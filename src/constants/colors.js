b2ebe16b960541f66fdbcc0bc0861b92dba5e3be
Abdul Basit <69422801+abdulbasit64@users.noreply.github.com>
2022-12-10 03:34:09 +0500

    Create colors.js

---
+++ src/constants/colors.js
@@ -0,0 +1,10 @@
+// Colors
+export const fontColor              = '#422f01';
+export const pinkShade              = '#F8C0B7';
+export const pinkShadeBg            = '#FFEDE2';
+export const backgroundColor        = '#FCF5EB';
+
+export const white                  = '#FFFFFF';
+
+
+
