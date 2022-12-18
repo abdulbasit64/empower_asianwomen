c87b124a11e8de6dc1d93fdd0a3747aff6ee0a3e
Abdul Basit <69422801+abdulbasit64@users.noreply.github.com>
2022-12-18 10:10:20 +0500

    Update Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,72 @@
 import React from 'react';
 import Button from '../components/button/Button';
-import { pinkShade, white } from '../constants/colors';
+import Card from '../components/card/Card';
+import Heading from '../components/heading/Heading';
+import { headingColor, pinkShade, white } from '../constants/colors';
+import {
+  aboutDesc,
+  aboutOurOrganistaion,
+  contentOrBlog,
+  introDesc,
+  media,
+  researchAndPlanning,
+  whatWeDo,
+  whatWeDoDesc,
+} from '../constants/content';
 
 function Home() {
   return (
     <div>
-      <section className='items-center lg:items-start'>
-        <h1 className='font-semibold my-5 text-4xl sm:text-5xl md:text-6xl lg:text-7xl' lang='en'>
+      <section className='items-center lg:items-start lg:w-4/5 '>
+        <h1
+          className='font-semibold text-center lg:text-left my-5 text-5xl sm:text-7xl md:text-7xl lg:text-8xl'
+          style={{ color: headingColor }}
+          lang='en'>
           Empowering Asian Women
         </h1>
-        <p id='intro' className='my-12'>
-          A non-profit, non-political and international organization set up in
-          2022 which aims at changing the roles of women in Asia through
-          encouragement, celebration and empowerment.
+        <p id='intro' className='my-12 md:text-left text-lg sm:text-xl'>
+          {introDesc}
         </p>
-        <Button text='Become a supporter' bg={pinkShade} color={white} />
+        <Button text='Become a part' bg={pinkShade} color={white} />
       </section>
-
-      <section className='my-6'>
-        <h2 className='text-3xl font-semibold my-2' lang='en'>
-          About our Organisation
-        </h2>
-        <p className='my-8 text-justify'>
-          Asian women may encounter a variety of challenges in realizing their
-          full potential as a result of cultural prejudices and preconceptions.
-          Many Asian women are making important contributions, both at home and
-          worldwide, yet they are not adequately acknowledged. Our objective is
-          to highlight these powerful women through our platform. Through
-          imaginative storytelling, we hope to foster a supportive environment
-          where Asian women may advance and serve as role models for younger
-          generations.
-        </p>
+      <section className='my-12'>
+        <div className='flex flex-col lg:flex-row gap-5'>
+          <div className='flex-1 flex flex-col justify-center gap-5'>
+            <Heading title={aboutOurOrganistaion} />
+            <p className='my-8 text-justify text-lg sm:text-xl'>{aboutDesc}</p>
+          </div>
+          <div className='flex flex-1'>
+            <img
+              src='src\assets\images\51lg.png'
+              className='object-cover'
+              alt='about_us_photo'></img>
+          </div>
+        </div>
       </section>
-
+      <section className='my-12 py-6'>
+        <Heading title={whatWeDo} alwaysCenter={true}/>
+        <p className='my-8 text-lg sm:text-xl'>{whatWeDoDesc}</p>
+        <div className='flex flex-col justify-between lg:flex-row items-center gap-9'>
+          <Card
+            imageSrc='src\assets\images\card1Cropped.png'
+            text='Learn More'
+            title={researchAndPlanning}
+            key={1}
+          />
+          <Card
+            imageSrc='src\assets\images\card2Cropped.png'
+            text='Learn More'
+            title={contentOrBlog}
+            key={2}
+          />
+          <Card
+            imageSrc='src\assets\images\card3Cropped.png'
+            text='Learn More'
+            title={media}
+            key={3}
+          />
+        </div>
+      </section>
     </div>
   );
 }
