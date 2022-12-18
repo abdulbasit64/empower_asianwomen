4a67418991c97a18c37590f278d81deea82f4a05
Abdul Basit <69422801+abdulbasit64@users.noreply.github.com>
2022-12-18 10:09:02 +0500

    add constants and colors

---
+++ src/constants/content.js
@@ -0,0 +1,24 @@
+// --- Heading and Titles
+export const whatWeDo = 'What We Do'
+export const aboutOurOrganistaion = 'About Our Organisation'
+export const researchAndPlanning = 'Research & Planning'
+export const contentOrBlog = 'Content / Blog'
+export const media = 'Media'
+
+// --- Landing Page Content
+export const introDesc = `A non-profit, non-political and international organization set up in
+2022 which aims at changing the roles of women in Asia through
+encouragement, celebration and empowerment.`;
+
+export const aboutDesc = `Asian women may encounter a variety of challenges in realizing their
+full potential as a result of cultural prejudices and preconceptions.
+Many Asian women are making important contributions, both at home and
+worldwide, yet they are not adequately acknowledged. Our objective is
+to highlight these powerful women through our platform. Through
+imaginative storytelling, we hope to foster a supportive environment
+where Asian women may advance and serve as role models for younger
+generations.`;
+
+export const whatWeDoDesc = `Empower Asian Women provides a network to powerful Asian women from across the world who want
+to see a society where women reach their full potential and use their influence to invest in younger
+generations while also empowering and developing the Asian community worldwide.`
