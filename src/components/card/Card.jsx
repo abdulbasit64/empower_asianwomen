b9967b4577025fa870a6315e8dd206e7c26d7c6e
Abdul Basit <69422801+abdulbasit64@users.noreply.github.com>
2022-12-18 14:30:41 +0500

    enhance Card component

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -7,15 +7,37 @@ import {
 } from '../../constants/colors';
 import Button from '../button/Button';
 
-function Card({imageSrc, title, text, to }) {
+function Card({ imageSrc, title, text, to, size, rounded, font }) {
   return (
-    <div className='flex flex-col rounded-3xl max-w-xs items-center gap-10 pb-3' style={{ backgroundColor: pinkShade }}>
+    <div
+      className={`flex flex-col ${rounded && 'rounded-3xl'} ${
+        size == 'small' ? 'max-w-xs pb-3' : 'max-w-md pb-8'
+      } items-center  gap-4 `}
+      style={{ backgroundColor: pinkShade }}>
       <img
         src={imageSrc}
-        className='object-cover rounded-3xl select-none outline-none'
+        className={`object-cover ${
+          rounded && 'rounded-3xl'
+        } select-none outline-none`}
         alt='card_cover_photo'></img>
-      <h2 className='text-2xl font-semibold' style={{ fontFamily: 'Arial' }}>{title}</h2>
-      {text && <Button text={text} bg={cardBtnColor} color={cardBtnFontColor} to={''} btnStyle="wide" />}
+      <div className='h-16 w-full flex items-center justify-center'>
+        <h2
+          className={`text-xl ${
+            size ? 'lg:text-2xl' : 'lg:text-3xl'
+          } font-semibold`}
+          style={{ fontFamily: !font ? 'Arial' : '' }}>
+          {title}
+        </h2>
+      </div>
+      {text && (
+        <Button
+          text={text}
+          bg={cardBtnColor}
+          color={cardBtnFontColor}
+          to={''}
+          btnStyle='wide'
+        />
+      )}
     </div>
   );
 }
