e3ad75f938575a4b3369a01b668aa715eba52eca
Abdul Basit <69422801+abdulbasit64@users.noreply.github.com>
2022-12-18 14:30:58 +0500

    add content

--- src/constants/content.js
+++ src/constants/content.js
@@ -1,9 +1,16 @@
 // --- Heading and Titles
-export const whatWeDo = 'What We Do'
-export const aboutOurOrganistaion = 'About Our Organisation'
-export const researchAndPlanning = 'Research & Planning'
-export const contentOrBlog = 'Content / Blog'
-export const media = 'Media'
+export const whatWeDo = `What We Do`;
+export const aboutOurOrganistaion = `About Our Organisation`;
+export const meetOurCoreTeam = `Meet Our Core Team`;
+export const ourWork = `Our Work`;
+export const newsAndUpdates = `News and Updates`;
+export const contactUs = `Contact Us`;
+
+export const researchAndPlanning = `Research & Planning`;
+export const contentOrBlog = `Content / Blog`;
+export const media = `Media`;
+export const researchAreas = `Research Areas`;
+export const seminars = `Seminars`;
 
 // --- Landing Page Content
 export const introDesc = `A non-profit, non-political and international organization set up in
@@ -21,4 +28,33 @@ generations.`;
 
 export const whatWeDoDesc = `Empower Asian Women provides a network to powerful Asian women from across the world who want
 to see a society where women reach their full potential and use their influence to invest in younger
-generations while also empowering and developing the Asian community worldwide.`
+generations while also empowering and developing the Asian community worldwide.`;
+
+// Core Team
+export const coreTeamMember1 = `Manahil Masood`;
+export const coreTeamMember2 = `Nehdiya Riaz`;
+export const coreTeamM1Desc = `Manahil Masood is an undergraduate
+student in Political Science at Kinnaird
+College. She is a research analyst and
+interested in effective policymaking and
+implementation regarding rights and
+freedoms of minorities in society,
+particularly women and children.`;
+
+export const coreTeamM2Desc = `Nehdiya Riaz is a post-grad student at
+the University of Manchester pursuing
+her Masters in Financial Management.
+She is enthusiastically interested in
+financial management both as a career
+path. Her areas of expertise include
+accounting, finance and data sciences.`;
+
+// News And Updates
+export const newsCard1 = `OUR 10TH ANNIVERSARY`;
+export const newsCard2 = `THE ASIAN OUTREACH PROGRAM`;
+export const newsCard3 = `GRAND RELAUNCH 2025`;
+export const newsCardDesc = `Showcase your work by providing sample
+projects your company has completed. This
+will give potential clients a sneak peek of
+what you do.
+`;
