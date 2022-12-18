6a6f2c1cdee51152da1873c5b5b915eebd41eb50
Abdul Basit <69422801+abdulbasit64@users.noreply.github.com>
2022-12-18 10:08:03 +0500

    enhance button component

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Styles from './button.module.css';
 
-function Button(props) {
-	return (
-		<div className='rounded-full px-5 py-3 m-2 w-fit' style={{backgroundColor: props.bg}}>
-			<p className='text-xs' style={{fontFamily: 'Arial', color: props.color}}>{props.text}</p>
-		</div>
-	)
+function Button({text, to, bg, color, btnStyle}) {
+  return (
+    <a href={to}>
+      <div
+        className={[`rounded-full  px-7 ${btnStyle=='wide' ? 'py-2 px-16' : 'py-4'} m-2 w-fit`, Styles.btn].join(' ')}
+        style={{ backgroundColor: bg, paddingTop: btnStyle }}>
+        <p
+          className={`text-base ${btnStyle=='wide' ? 'font-semibold' : 'font-normal'} uppercase`}
+          style={{ fontFamily: 'Arial', color: color }}>
+          {text}
+        </p>
+      </div>
+    </a>
+  );
 }
 
-export default Button
+export default Button;
