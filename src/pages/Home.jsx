09cf6613f726e2dbf7d8ee2cee0a5dca380fedc7
Abdul Basit <69422801+abdulbasit64@users.noreply.github.com>
2022-12-18 14:31:20 +0500

    Update Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,27 @@ import React from 'react';
 import Button from '../components/button/Button';
 import Card from '../components/card/Card';
 import Heading from '../components/heading/Heading';
-import { headingColor, pinkShade, white } from '../constants/colors';
+import { headingColor, pinkShade, pinkShadeBg, white } from '../constants/colors';
 import {
   aboutDesc,
   aboutOurOrganistaion,
   contentOrBlog,
+  coreTeamM1Desc,
+  coreTeamM2Desc,
+  coreTeamMember1,
+  coreTeamMember2,
   introDesc,
   media,
+  meetOurCoreTeam,
+  newsAndUpdates,
+  newsCard1,
+  newsCard2,
+  newsCard3,
+  newsCardDesc,
+  ourWork,
   researchAndPlanning,
+  researchAreas,
+  seminars,
   whatWeDo,
   whatWeDoDesc,
 } from '../constants/content';
@@ -19,7 +32,7 @@ function Home() {
     <div>
       <section className='items-center lg:items-start lg:w-4/5 '>
         <h1
-          className='font-semibold text-center lg:text-left my-5 text-5xl sm:text-7xl md:text-7xl lg:text-8xl'
+          className='font-semibold text-center lg:text-left my-5 mt-10 text-5xl sm:text-7xl md:text-7xl lg:text-8xl'
           style={{ color: headingColor }}
           lang='en'>
           Empowering Asian Women
@@ -29,6 +42,7 @@ function Home() {
         </p>
         <Button text='Become a part' bg={pinkShade} color={white} />
       </section>
+
       <section className='my-12'>
         <div className='flex flex-col lg:flex-row gap-5'>
           <div className='flex-1 flex flex-col justify-center gap-5'>
@@ -39,32 +53,190 @@ function Home() {
             <img
               src='src\assets\images\51lg.png'
               className='object-cover'
-              alt='about_us_photo'></img>
+              alt='about_us_photo'
+            />
           </div>
         </div>
       </section>
       <section className='my-12 py-6'>
-        <Heading title={whatWeDo} alwaysCenter={true}/>
-        <p className='my-8 text-lg sm:text-xl'>{whatWeDoDesc}</p>
-        <div className='flex flex-col justify-between lg:flex-row items-center gap-9'>
+        <Heading title={whatWeDo} alwaysCenter={true} />
+        <p className='my-8 mt-10 text-lg sm:text-xl'>{whatWeDoDesc}</p>
+        <div className='flex flex-col justify-between lg:flex-row items-center gap-10 lg:gap-32 basis-8 mt-4'>
           <Card
-            imageSrc='src\assets\images\card1Cropped.png'
+            imageSrc='src\assets\images\card1.png'
+            size='small'
+            rounded={true}
             text='Learn More'
             title={researchAndPlanning}
-            key={1}
+            key={researchAndPlanning}
           />
           <Card
-            imageSrc='src\assets\images\card2Cropped.png'
+            imageSrc='src\assets\images\card2.png'
+            size='small'
+            rounded={true}
             text='Learn More'
             title={contentOrBlog}
-            key={2}
+            key={contentOrBlog}
           />
           <Card
-            imageSrc='src\assets\images\card3Cropped.png'
+            imageSrc='src\assets\images\card3.png'
+            size='small'
+            rounded={true}
             text='Learn More'
             title={media}
-            key={3}
+            key={media}
           />
+        </div>
+      </section>
+
+      <section className='my-12 py-6'>
+        <Heading title={meetOurCoreTeam} alwaysCenter={true} />
+        <div className='flex flex-col justify-around items-center mt-10 lg:flex-row'>
+          {/* Member 1 */}
+          <div className='flex flex-col items-center w-72 mb-6'>
+            <img
+              src='src\assets\images\coreM1.webp'
+              className='object-cover rounded-full mb-4 w-40 lg:mb-8 lg:w-48'
+              alt='core_team_member_1'
+            />
+            <h4
+              className='text-2xl font-semibold mb-3'
+              style={{ color: headingColor }}>
+              {coreTeamMember1}
+            </h4>
+            <p
+              className='text-justify leading-tight'
+              style={{ fontFamily: 'Arial', color: headingColor }}>
+              {coreTeamM1Desc}
+            </p>
+          </div>
+          {/* Member 2 */}
+          <div className='flex flex-col items-center w-72 mb-6'>
+            <img
+              src='src\assets\images\coreM2.webp'
+              className='object-cover rounded-full mt-4 mb-4 w-40 lg:mb-8 lg:w-48 lg:mt-0'
+              alt='core_team_member_1'
+            />
+            <h4
+              className='text-2xl font-semibold mb-3'
+              style={{ color: headingColor }}>
+              {coreTeamMember2}
+            </h4>
+            <p
+              className='text-justify leading-tight'
+              style={{ fontFamily: 'Arial', color: headingColor }}>
+              {coreTeamM2Desc}
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section className='my-12 py-6'>
+        <Heading title={ourWork} alwaysCenter={true} />
+        <div className='flex flex-col justify-evenly lg:flex-row items-center gap-9 my-6 mt-10'>
+          <Card
+            imageSrc='src\assets\images\card4.png'
+            rounded={true}
+            font={'inherit'}
+            title={researchAreas}
+            key={researchAreas}
+          />
+          <Card
+            imageSrc='src\assets\images\card5.png'
+            rounded={true}
+            font={'inherit'}
+            title={seminars}
+            key={seminars}
+          />
+        </div>
+      </section>
+
+      <section className='my-12 py-6'>
+        <Heading title={newsAndUpdates} alwaysCenter={true} />
+        <div className='flex flex-col lg:flex-row justify-center items-center gap-10 basis-8 mt-10'>
+          <div className='max-w-xs'>
+            <Card
+              imageSrc='src\assets\images\card4.png'
+              size={'small'}
+              font={'inherit'}
+              title={newsCard1}
+              key={researchAreas}
+            />
+            <p className='mt-6 text-center'>{newsCardDesc}</p>
+          </div>
+          <div className='max-w-xs'>
+            <Card
+              imageSrc='src\assets\images\card4.png'
+              size={'small'}
+              font={'inherit'}
+              title={newsCard2}
+              key={researchAreas}
+            />
+            <p className='mt-6 text-center'>{newsCardDesc}</p>
+          </div>
+          <div className='max-w-xs'>
+            <Card
+              imageSrc='src\assets\images\card4.png'
+              size={'small'}
+              font={'inherit'}
+              title={newsCard3}
+              key={researchAreas}
+            />
+            <p className='mt-6 text-center'>{newsCardDesc}</p>
+          </div>
+        </div>
+      </section>
+
+      <section className='my-12 pb-6'>
+        <div className='full-width flex justify-center mb-10' style={{backgroundColor: pinkShadeBg}}>
+          <img src='src/assets/images/contactUsBg.png' className='object-cover'/>
+        </div>
+        <Heading title='Contact Us'/>
+        {/* Social Media And Email Links */}
+        <div className='flex flex-col sm:flex-row justify-between mt-8'>
+          <div className='text-left'>
+            <h4 className='text-2xl font-bold' style={{ color: headingColor }}>
+              Social Media
+            </h4>
+            <div className='flex my-2'>
+              <img
+                className='mr-2'
+                width='24'
+                src='src\assets\images\ig-logo.png'
+                alt='instagram_logo'
+              />
+              <a
+                href='https://www.instagram.com/empowerasianwomen'
+                style={{ color: headingColor }}
+                target='_blank'>
+                @empowerasianwomen
+              </a>
+            </div>
+            <div className='flex'>
+              <img
+                className='mr-2'
+                width='24'
+                src='src\assets\images\twitter-logo.png'
+                alt='twitter_logo'
+              />
+              <a
+                href='https://twitter.com/empowerasian'
+                style={{ color: headingColor }}
+                target='_blank'>
+                @empowerasian
+              </a>
+            </div>
+          </div>
+          <div className='text-left mt-5 sm:mt-0'>
+            <h4 className='text-2xl font-bold' style={{ color: headingColor }}>
+              Email Address
+            </h4>
+            <a
+              style={{ color: headingColor }}
+              href='mailto: empowerasianwomen@gmail.com'>
+              empowerasianwomen@gmail.com
+            </a>
+          </div>
         </div>
       </section>
     </div>
